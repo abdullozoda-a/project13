d3fcefb1ddc7ebf726dff1bffc9184f5c1df5e54
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-04 17:58:38 +0500

    create project

---
+++ project13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            int i = 1;
+            while (n >= 1)
+            {
+                i *= 0;
+                n -= 2;
+
+            }
+            Console.WriteLine($"Двойной факториал равен {i}");
+        }
+    }
+}
